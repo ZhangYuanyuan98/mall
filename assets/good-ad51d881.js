6febbf17968458b8528f462781ce116ec9b7ff29
ZhangYuanyuan98 <yy1079116013@163.com>
2025-11-11 23:05:43 +0800

    deploy: 添加 nojekyll 修复

---
+++ assets/good-ad51d881.js
@@ -0,0 +1,1 @@
+import{y as e}from"./index-e521f136.js";function a(t){return e.get(`/goods/detail/${t}`)}function o(){return e.get("/categories")}function s(t){return e.get("/search",{params:t})}export{a,o as g,s};
